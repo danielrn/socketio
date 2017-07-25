d88a266bed182425a43297353b2f7aae8c3ceea5
daniel <dreyes@aqtiva.mx>
2017-07-24 19:58:49 -0500

    Se inserto estilos formulario

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var app = require('express')();//app = express();
+var app = require('express')();
+var express = require('express');
+
+var path = require('path');
 var http = require('http').createServer(app);
 
+app.use(express.static(path.join(__dirname, 'css')));
+
 app.get('/',function(req,res){
-    res.send('<h1>Hola mundo</h1>');
+    //res.send('<h1>Hola mundo</h1>');
+    res.sendFile(__dirname+'/index.html');
 });
 
 http.listen(3000,function(){
